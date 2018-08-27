79707421eb6ac8d8e97ad9d13f228addf3336325
Jay Griggs <jaygriggs1976@gmail.com>
2018-08-27 10:28:03 -0400

    Final Version

---
+++ FunTimer/FunTimer/APA102C.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+using GHIElectronics.TinyCLR.Devices.Spi;
+
+namespace GHIElectronics.TinyCLR.Drivers.ShijiLighting.APA102C
+{
+    public class APA102C
+    {
+        private SpiController spiBusContoller;
+        private SpiDevice spiBus;
+        private readonly string spiID;
+        private readonly SpiConnectionSettings spiSettings;
+        private readonly byte[] startFrame;
+        private readonly byte[] stopFrame;
+        private byte[] ledFrame;
+        private readonly int pixelCount;
+        private int ledFrameIndex;
+        private int pixelIntensity;
+
+
+
+
+        public APA102C(int pixelCount, string spiID)
+        {
+            this.pixelCount = pixelCount;
+            startFrame = new byte[4];                           // Initializes all elements in startFrame array to 0x0
+            stopFrame = new byte[] { 0xff, 0xff, 0xff, 0xff };
+            ledFrame = new byte[this.pixelCount * 4];           // Sets up 32 bit data buffer per each LED Frame (pixel)
+
+            for (int i = 0; i < ledFrame.Length; i += 4)        // Initializes frame buffer to active LED frame data
+            {
+                ledFrame[i] = 0xE0;
+            }
+
+            spiSettings = new SpiConnectionSettings(7)
+            {
+                Mode = SpiMode.Mode0,
+                ClockFrequency = 1_200_000,
+                DataBitLength = 8
+            };
+
+            this.spiID = spiID;
+            spiBusContoller = SpiController.FromName(this.spiID);
+            spiBus = spiBusContoller.GetDevice(spiSettings);
+        }
+        /*
+        static public short Set16bitColor(byte Red, byte Green, byte Blue)
+        {
+            if (Red > 0x1f) throw new Exception();
+            if (Green > 0x2f) throw new Exception();
+
+            short color = 0;
+            color = (short)((Blue & 0xF8) >> 3);
+            color |= (short)(((Green & 0xFC) >> 2) << 5);
+            color |= (short)(((Red & 0xF8) >> 3) << 11);
+            return color;
+        }
+        */
+
+        //static public int SetColor(byte Red, byte Green, byte Blue)
+        //{
+        //    int color;
+        //    color = Red << 16;
+        //    color |= Green << 8;
+        //    color |= Blue << 0;
+        //    return color;
+        //}
+
+        /// <param name="pixelIndex">The pixel in the chain to draw</param>
+        /// <param name="colorData16bit565">The 16 bit color to draw. It is in the RGB 565 format.</param>
+        /// <param name="pixelIntensity">The level from 0 (off) to 31 (highest brightness)</param>
+        public void SetLED(int pixelIndex, int red, int green, int blue, int pixelIntensity = 1)
+        {
+            if (pixelCount < pixelIndex)
+            {
+                throw new Exception("Pixel Index is out of range of Pixel Count.");
+            }
+
+            ledFrameIndex = pixelIndex * 4; // Positions index to beginning of each LED frame
+
+            this.pixelIntensity = pixelIntensity;
+            this.pixelIntensity |= 0x7 << 5;
+
+            ledFrame[ledFrameIndex] = (byte)this.pixelIntensity;
+            ledFrame[ledFrameIndex + 1] = (byte)(blue);         // Blue byte
+            ledFrame[ledFrameIndex + 2] = (byte)(green);        // Green byte
+            ledFrame[ledFrameIndex + 3] = (byte)(red);          // Red byte
+        }
+
+        public void RefreshLEDs()
+        {
+            // The startFrame sends all zeros first in the chain to let the addressable LEDs know the next set of data is the LED Frame Data field.
+            spiBus.Write(startFrame);
+            // ledFrame is the entire pixel chain data.
+            spiBus.Write(ledFrame);
+            // Unknown if this is necessary
+            spiBus.Write(stopFrame);
+        }
+    }
+}
---
+++ FunTimer/FunTimer/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+using GHIElectronics.TinyCLR.Drivers.ShijiLighting.APA102C;
+
+namespace FunTimer
+{
+    class Program
+    {
+        public static void LEDThread()
+        {
+            while (true)
+            {
+                BrainPad.LightBulb.TurnRed();
+                BrainPad.Wait.Seconds(0.5);
+                BrainPad.LightBulb.TurnOff();
+                BrainPad.Wait.Seconds(0.5);
+            }
+        }
+       
+        const int _LedCount = 25;
+        static APA102C Strip = new APA102C(_LedCount, GHIElectronics.TinyCLR.Pins.BrainPad.Expansion.SpiBus.Spi1);
+
+        static void ControlLEDs(int count)
+        {
+
+                //clear all leds
+                for ( int i = 0; i < _LedCount; i++)
+                {
+                    Strip.SetLED(i, 0, 0, 0, 5);
+                }
+            //set the "count" to on
+            for ( int i = 0; i < count; i++)
+                {
+                    double color = ((double)i / _LedCount) * 255;
+                    Strip.SetLED(i, (int)color, (int)(255 - color), 0, 5);
+                }
+                //refresh
+                Strip.RefreshLEDs();
+        }
+
+        static void Main()
+        {
+            
+            Thread Blinker = new Thread(LEDThread);
+            Blinker.Start();
+            int delaytime = 60;
+            int currenttime = delaytime;
+
+            while (true)
+            {
+
+                if (BrainPad.Buttons.IsDownPressed())
+                {
+                    currenttime = 60;
+                }
+                BrainPad.Display.DrawText(0, 0, currenttime.ToString());
+                BrainPad.Display.RefreshScreen();             
+                int howmanyledstoturnon = (int)(((double)currenttime / delaytime * _LedCount));
+                currenttime--;
+                ControlLEDs(howmanyledstoturnon);
+                Thread.Sleep(500);
+                ControlLEDs(howmanyledstoturnon--);
+                Thread.Sleep(500);
+                ControlLEDs(howmanyledstoturnon++);
+
+
+                if (currenttime<=10 && currenttime >= 0 )
+                  {
+              BrainPad.Buzzer.Beep();
+
+                  }
+               if (currenttime <= 0)
+                {
+                    for (int i = 0; i < _LedCount; i++)
+                    {
+                        Strip.SetLED(i, 0, 0, 255, 5);
+                    }
+                    Strip.RefreshLEDs();
+                }
+            }
+
+        }
+
+      
+    }
+}
